c88e5c88421b492500e748a283e426a407567797
Gina <ginal@indiefin.com>
2020-04-14 12:33:05 +0200

    Styles commit
    CSS, JS and image added

---
+++ react.js
@@ -0,0 +1,12 @@
+ReactDOM.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById("root")
+);
+ReactDOM.render(
+  React.createElement(
+    "p",
+    null,
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae est sed lacus venenatis varius eu vel mi. Ut arcu magna, euismod in consectetur sit amet, mollis non libero. Donec faucibus vitae nisi at bibendum. Nulla tincidunt sed nulla eget condimentum. Praesent facilisis nisi vitae velit dignissim, ac dictum velit lacinia. Maecenas ut quam eget odio egestas aliquet. Nullam non libero lacinia, aliquam dui vitae, fringilla lorem. Aliquam erat volutpat. Ut cursus felis ut odio luctus, vel iaculis nisi tempus. Nunc semper augue eget velit varius cursus. In posuere consequat quam at varius. Proin eleifend suscipit dapibus."
+  ),
+  document.getElementById("root2")
+);
